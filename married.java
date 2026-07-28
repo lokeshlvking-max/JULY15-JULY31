c0aa6522beb6b46ffe0a1085541a734e11d0dcbd
lokeshlvking-max <lokeshlvking@gmail.com>
2026-07-27 22:03:09 -1200

    Create married.java

---
+++ married.java
@@ -0,0 +1,14 @@
+ public class Main{
+    public static void main(String[]args){
+        String str="loki";
+        String str1="pavi";
+        char[]arr=str.toCharArray();
+        char[]arr1=str1.toCharArray();
+        if(arr.length!=arr1.length)
+           {
+                System.out.println("unmarried");
+                return;
+           }
+            System.out.println("married");
+    }
+ }marrid
